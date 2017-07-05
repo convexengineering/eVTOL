cccc69ebae8a924a877deb37b238eb010b417672
Arthur Brown <arthurb@mit.edu>
2017-07-05 11:43:38 -0400

    File reorganization

--- vehicle_parameters/structural_fraction/structural_fraction.py
+++ vehicle_parameters/structural_fraction/structural_fraction.py
@@ -1,8 +1,8 @@
-# Sweep to evaluate the sensitivity of each design to battery energy density
+# Sweep to evaluate the sensitivity of each design to structural mass fraction
 
 import os
 import sys
-sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + '..'))
+sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/../..'))
 
 import numpy as np
 from gpkit import Model, ureg
@@ -15,7 +15,7 @@ from study_input_data import generic_data, configuration_data
 #General data
 eta_cruise = generic_data["\eta_{cruise}"] 
 eta_electric = generic_data["\eta_{electric}"]
-weight_fraction = generic_data["weight_fraction"]
+#weight_fraction = generic_data["weight_fraction"]
 C_m = generic_data["C_m"]
 n = generic_data["n"]
 
@@ -45,28 +45,20 @@ deadhead_N_passengers = generic_data["deadhead_mission"]["N_passengers"]
 deadhead_mission_range = generic_data["deadhead_mission"]["range"]
 deadhead_time_in_hover = generic_data["deadhead_mission"]["time_in_hover"]
 
-# Delete certain configurations
+# Delete some configurations
 configs = configuration_data.copy()
 del configs["Tilt duct"]
 del configs["Multirotor"]
 del configs["Autogyro"]
 
+#Data specific to study
+weight_fraction = np.linspace(0.4,0.6,10) #structural mass fraction (sweep)
+
+
 #Optimize remaining configurations
 for config in configs:
-
+	
 	print "Solving configuration: " + config
-
-	#set up C_m arrays (different for each configuration)
-	if config == "Helicopter":
-		C_m = np.linspace(350,600,10)*ureg.Wh/ureg.kg
-	elif config == "Coaxial heli":
-		C_m = np.linspace(300,600,11)*ureg.Wh/ureg.kg
-	elif config == "Lift + cruise":
-		C_m = np.linspace(250,600,13)*ureg.Wh/ureg.kg
-	else:
-		C_m = np.linspace(200,600,15)*ureg.Wh/ureg.kg
-
-	C_m = ("sweep",C_m)
 
 	c = configs[config]
 
@@ -96,7 +88,7 @@ for config in configs:
 		charger_power=charger_power,mission_type=deadhead_mission_type)
 
 	MissionCost = OnDemandMissionCost(Aircraft,RevenueMission,DeadheadMission,
-		pilot_wrap_rate=pilot_wrap_rate,mechanic_wrap_rate=mechanic_wrap_rate,MMH_FH=MMH_FH,\
+		pilot_wrap_rate=pilot_wrap_rate,mechanic_wrap_rate=mechanic_wrap_rate,MMH_FH=MMH_FH,
 		deadhead_ratio=deadhead_ratio)
 
 	problem = Model(MissionCost["cost_per_trip"],
@@ -106,17 +98,18 @@ for config in configs:
 
 	configs[config]["solution"] = solution
 
-	configs[config]["C_m_array"] = solution("C_m_OnDemandAircraft/Battery")
+	configs[config]["weight_fraction"] = solution("weight_fraction_OnDemandAircraft/Structure")
 
 	configs[config]["MTOW"] = solution("MTOW_OnDemandAircraft")
 	configs[config]["W_{battery}"] = solution("W_OnDemandAircraft/Battery")
 	configs[config]["cost_per_trip_per_passenger"] = solution("cost_per_trip_per_passenger_OnDemandMissionCost")
 	configs[config]["SPL"] = 20*np.log10(solution("p_{ratio}_OnDemandSizingMission"))
-		
+
 
 # Plotting commands
 plt.ion()
 fig1 = plt.figure(figsize=(17,11), dpi=80)
+plt.rc('axes', axisbelow=True)
 plt.show()
 
 style = {}
@@ -125,63 +118,62 @@ style["marker"] = ["s","o","^","v","s","o","^","v"]
 style["fillstyle"] = ["full","full","full","full","none","none","none","none"]
 style["markersize"] = 10
 
-C_m = C_m[1] #convert back to array from tuple
-
 #Maximum takeoff weight
 plt.subplot(2,2,1)
 for i, config in enumerate(configs):
 	c = configs[config]
-	plt.plot(c["C_m_array"].to(ureg.Wh/ureg.kg).magnitude,c["MTOW"].to(ureg.lbf).magnitude,
+	plt.plot(c["weight_fraction"],c["MTOW"].to(ureg.lbf).magnitude,
 		color="black",linewidth=1.5,linestyle=style["linestyle"][i],marker=style["marker"][i],
 		fillstyle=style["fillstyle"][i],markersize=style["markersize"],label=config)
 plt.grid()
 plt.ylim(ymin=0)
-plt.xlabel('Battery energy density (Wh/kg)', fontsize = 16)
+plt.xlabel('Structural mass fraction', fontsize = 16)
 plt.ylabel('Weight (lbf)', fontsize = 16)
 plt.title("Maximum Takeoff Weight",fontsize = 20)
-plt.legend(numpoints = 1,loc='upper right', fontsize = 12)
+plt.legend(numpoints = 1,loc='upper left', fontsize = 12)
 
 #Battery weight
 plt.subplot(2,2,2)
 for i, config in enumerate(configs):
 	c = configs[config]
-	plt.plot(c["C_m_array"].to(ureg.Wh/ureg.kg).magnitude,c["W_{battery}"].to(ureg.lbf).magnitude,
+	plt.plot(c["weight_fraction"],c["W_{battery}"].to(ureg.lbf).magnitude,
 		color="black",linewidth=1.5,linestyle=style["linestyle"][i],marker=style["marker"][i],
 		fillstyle=style["fillstyle"][i],markersize=style["markersize"],label=config)
 plt.grid()
 plt.ylim(ymin=0)
-plt.xlabel('Battery energy density (Wh/kg)', fontsize = 16)
+plt.xlabel('Structural mass fraction', fontsize = 16)
 plt.ylabel('Weight (lbf)', fontsize = 16)
 plt.title("Battery Weight",fontsize = 20)
-plt.legend(numpoints = 1,loc='upper right', fontsize = 12)
+plt.legend(numpoints = 1,loc='upper left', fontsize = 12)
+
 
 #Trip cost per passenger
 plt.subplot(2,2,3)
 for i, config in enumerate(configs):
 	c = configs[config]
-	plt.plot(c["C_m_array"].to(ureg.Wh/ureg.kg).magnitude,c["cost_per_trip_per_passenger"],
+	plt.plot(c["weight_fraction"],c["cost_per_trip_per_passenger"],
 		color="black",linewidth=1.5,linestyle=style["linestyle"][i],marker=style["marker"][i],
 		fillstyle=style["fillstyle"][i],markersize=style["markersize"],label=config)
 plt.grid()
 plt.ylim(ymin=0)
-plt.xlabel('Battery energy density (Wh/kg)', fontsize = 16)
+plt.xlabel('Structural mass fraction', fontsize = 16)
 plt.ylabel('Cost ($US)', fontsize = 16)
 plt.title("Cost per Trip, per Passenger",fontsize = 20)
-plt.legend(numpoints = 1,loc='upper right', fontsize = 12)
+plt.legend(numpoints = 1,loc='upper left', fontsize = 12)
+
 
 #Sound pressure level (in hover)
 plt.subplot(2,2,4)
 for i, config in enumerate(configs):
 	c = configs[config]
-	plt.plot(c["C_m_array"].to(ureg.Wh/ureg.kg).magnitude,c["SPL"],
+	plt.plot(c["weight_fraction"],c["SPL"],
 		color="black",linewidth=1.5,linestyle=style["linestyle"][i],marker=style["marker"][i],
 		fillstyle=style["fillstyle"][i],markersize=style["markersize"],label=config)
 plt.grid()
-plt.xlabel('Battery energy density (Wh/kg)', fontsize = 16)
+plt.xlabel('Structural mass fraction', fontsize = 16)
 plt.ylabel('SPL (dB)', fontsize = 16)
 plt.title("Sound Pressure Level in Hover (sizing mission)",fontsize = 20)
-plt.legend(numpoints = 1,loc='upper right', fontsize = 12)
-
+plt.legend(numpoints = 1,loc='upper left', fontsize = 12)
 
 if reserve_type == "FAA":
 	num = solution["constants"]["t_{loiter}_OnDemandSizingMission"].to(ureg.minute).magnitude
@@ -195,8 +187,8 @@ if autonomousEnabled:
 else:
 	autonomy_string = "pilot required"
 
-title_str = "Aircraft parameters: structural mass fraction = %0.2f; %s\n" \
-	% (weight_fraction, autonomy_string) \
+title_str = "Aircraft parameters: battery energy density = %0.0f Wh/kg; %s\n" \
+	% (C_m.to(ureg.Wh/ureg.kg).magnitude, autonomy_string) \
 	+ "Sizing mission (%s): range = %0.0f nm; %0.0f passengers; %0.0fs hover time; reserve type = " \
 	% (sizing_mission_type, sizing_mission_range.to(ureg.nautical_mile).magnitude, sizing_N_passengers, sizing_time_in_hover.to(ureg.s).magnitude) \
 	+ reserve_type + reserve_type_string + "\n"\
@@ -210,4 +202,4 @@ title_str = "Aircraft parameters: structural mass fraction = %0.2f; %s\n" \
 plt.suptitle(title_str,fontsize = 16)
 
 plt.tight_layout()#makes sure subplots are spaced neatly
-plt.subplots_adjust(left=0.05,right=0.95,bottom=0.05,top=0.86)#adds space at the top for the title
+plt.subplots_adjust(left=0.05,right=0.95,bottom=0.05,top=0.85)#adds space at the top for the title
